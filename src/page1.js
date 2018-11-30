9f01e54f4ed8190c4814775550f357dc0a431d7d
Dennis Frolov <denn.frolov@gmail.com>
2018-11-30 16:14:36 -0500

    updated new file

--- src/page1.js
+++ src/page1.js
@@ -1,1 +1,24 @@
-hflksdafhlsjhlkdj
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class List extends React.Component {
+	render() {
+		return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+	}
+}
+
+// ========================================
+
+ReactDOM.render(
+  <List name="Dennis" />,
+  document.getElementById('root')
+);
