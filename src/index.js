d6954e6d9bf6a36907dd9245385609ebeb22d30f
Unknown <m3teitel@ryerson.ca>
2018-11-30 17:05:56 -0500

    changes to link

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class List extends React.Component {
+import { Link } from 'react-router-dom';
+
+class List1 extends React.Component {
 	render() {
 		return (
       <div className="shopping-list">
         <h1>Shopping List for {this.props.name}</h1>
         <ul>
-          <li><a href="page1.js">Page1</a></li>
+          <li><Link to="/page1">Page1</Link></li>
         </ul>
       </div>
     );
@@ -16,6 +18,6 @@ class List extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <List name="CPS530" />,
+  <List1 name="CPS530" />,
   document.getElementById('root')
 );
