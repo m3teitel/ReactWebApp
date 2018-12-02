f0a7b6c680dd22af1eb9d4eda15f31a3eb5d05a0
Dennis Frolov <denn.frolov@gmail.com>
2018-12-02 18:14:29 -0500

    editing tutorial page

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -8,12 +8,15 @@ class Tutorial extends React.Component {
 			<html>
       <div className="parallax"></div>
 			<div className="bg-text">
-        <h1>Tutorial Page</h1>
+        <h1>Best tutorial ever fricking made</h1>
 				<br/>
 				<body>
 					<p>
-						This is a Tutorial of React and Node.js.
+						Y'all ready to learn how to make the best darn website on this side of the galaxy?
 					</p>
+					<div>
+						First we gonna show you how to make some methods n shizzle
+					</div>
 				</body>
       </div>
 			</html>
