0ecd65b8b25500e264f3c2dbd8aeb9624725b378
Dennis Frolov <denn.frolov@gmail.com>
2018-12-03 03:11:32 -0500

    halfway done webpage

--- src/WebPage.js
+++ src/WebPage.js
@@ -11,16 +11,16 @@ import mercury from './Mercury.jpg';
 class WebPage extends React.Component {
 	render() {
 		return (
-			<html>
+			<html class="gradient">
 				<title>My First React Page</title>
-				<body class="page">
-					<h1>Best Planets Around</h1>
+				<body class="bg-textwebpage">
+					<h1 align="center">Best Planets Around</h1>
 					<ol>
-						<li>Saturn<br/><img src={saturn} alt="Saturn" height="50px"/></li>
-						<li>Uranus<br/><img src={uranus} alt="Uranus" height="50px"/></li>
-						<li>Venus<br/><img src={venus} alt="Venus" height="50px"/></li>
-						<li>Neptune<br/><img src={neptune} alt="Neptune" height="50px"/></li>
-						<li>Mercury<br/><img src={mercury} alt="Mercury" height="50px"/></li>
+						<li>Saturn<br/><img class="move" src={saturn} alt="Saturn" height="50px"/></li>
+						<li>Uranus<br/><img class="move" src={uranus} alt="Uranus" height="50px"/></li>
+						<li>Venus<br/><img class="move" src={venus} alt="Venus" height="50px"/></li>
+						<li>Neptune<br/><img class="move" src={neptune} alt="Neptune" height="50px"/></li>
+						<li>Mercury<br/><img class="move" src={mercury} alt="Mercury" height="50px"/></li>
 					</ol>
 				</body>
 			</html>
