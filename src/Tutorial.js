0ada1c269be18ed23201c4317ec28eb9df62e1b0
Dennis Frolov <denn.frolov@gmail.com>
2018-12-02 19:52:55 -0500

    testing linking to reddit

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -4,23 +4,55 @@ import './Tutorial.css'
 
 class Tutorial extends React.Component {
 	render() {
+		var redditSubModule = "SpacePorn";
+		var http = require('http');
+		var name = "";
+
+		function getRedditPosts() {
+			var url = "http://www.reddit.com/r/" + redditSubModule + "/new/.json?limit=50";
+
+			var request = http.get(url, function(response) {
+				var json = '';
+				response.on('data', function(chunk) {
+					json += chunk;
+				});
+
+				response.on('end', function() {
+					var redditResponse = JSON.parse(json);
+					redditResponse.data.children.forEach(function(child) {
+			if(child.data.domain !== 'self.node') {
+				this.name += '-------------------------------';
+				this.name += 'Author : ' + child.data.author;
+				this.name += 'Domain : ' + child.data.domain;
+				this.name += 'Title : ' + child.data.title;
+				this.name += 'URL : ' + child.data.url;
+			}
+					});
+				})
+			});
+			request.on('error', function(err) {
+				console.log(err);
+			});
+		}
+		getRedditPosts().
 		return (
 			<html>
-      <div className="parallax"></div>
-			<div className="bg-text">
-        <h1>Best tutorial ever fricking made</h1>
-				<br/>
-				<body>
-					<p>
-						Y'all ready to learn how to make the best darn website on this side of the galaxy?
-					</p>
-					<div>
-						First we gonna show you how to make some methods n shizzle
-					</div>
-				</body>
-      </div>
+				<div className="parallax"></div>
+				<div className="bg-text">
+					<h1>Best tutorial ever fricking made</h1>
+					<br/>
+					<body>
+						<p>
+							Y'all ready to learn how to make the best darn website on this side of the galaxy?
+						</p>
+						<div>
+							this.name
+						</div>
+						<br/>
+					</body>
+				</div>
 			</html>
-    );
+    	);
 	}
 }
 
