bd92235eed0c15e60026c15e4d946999a7a5c03e
Dennis Frolov <denn.frolov@gmail.com>
2018-12-02 20:01:51 -0500

    moved code around trying to get reddit working

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -2,33 +2,26 @@ import React from 'react';
 import './index.css';
 import './Tutorial.css'
 
-class Tutorial extends React.Component {
-	render() {
-		return (
-			<html>
-			var redditSubModule = "SpacePorn";
+var redditSubModule = "SpacePorn";
 			var http = require('http');
 			var name = "";
-
 			function getRedditPosts() {
 				var url = "http://www.reddit.com/r/" + redditSubModule + "/new/.json?limit=50";
-
 				var request = http.get(url, function(response) {
 					var json = '';
 					response.on('data', function(chunk) {
 						json += chunk;
 					});
-
 					response.on('end', function() {
 						var redditResponse = JSON.parse(json);
 						redditResponse.data.children.forEach(function(child) {
-				if(child.data.domain !== 'self.node') {
-					this.name += '-------------------------------';
-					this.name += 'Author : ' + child.data.author;
-					this.name += 'Domain : ' + child.data.domain;
-					this.name += 'Title : ' + child.data.title;
-					this.name += 'URL : ' + child.data.url;
-				}
+							if(child.data.domain !== 'self.node') {
+								this.name += '-------------------------------';
+								this.name += 'Author : ' + child.data.author;
+								this.name += 'Domain : ' + child.data.domain;
+								this.name += 'Title : ' + child.data.title;
+								this.name += 'URL : ' + child.data.url;
+							}
 						});
 					})
 				});
@@ -37,6 +30,10 @@ class Tutorial extends React.Component {
 				});
 			}
 			getRedditPosts().
+class Tutorial extends React.Component {
+	render() {
+		return (
+			<html>
 				<div className="parallax"></div>
 				<div className="bg-text">
 					<h1>Best tutorial ever fricking made</h1>
