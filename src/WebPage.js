80e93604ac3f848a7b51931fa4249300e3c36169
Dennis Frolov <denn.frolov@gmail.com>
2018-12-03 04:54:42 -0500

    done webpage

--- src/WebPage.js
+++ src/WebPage.js
@@ -9,13 +9,18 @@ import mercury from './Mercury.jpg';
 
 
 class WebPage extends React.Component {
+	trueClick(){
+		alert('CORRECT');
+	}
+	falseClick(){
+		alert('WRONG');
+	}
 	render() {
 		return (
 			<html class="gradient">
 				<title>My First React Page</title>
 				<body class="bg-textwebpage">
-				
-					<h1 align="center">Best Planets Around</h1>
+					<h1 class="header" align="center">Best Planets Around</h1>
 					<ol>
 						<li>Saturn<br/><img class="move" src={saturn} alt="Saturn" height="50px"/></li>
 						<li>Uranus<br/><img class="move" src={uranus} alt="Uranus" height="50px"/></li>
@@ -23,6 +28,21 @@ class WebPage extends React.Component {
 						<li>Neptune<br/><img class="move" src={neptune} alt="Neptune" height="50px"/></li>
 						<li>Mercury<br/><img class="move" src={mercury} alt="Mercury" height="50px"/></li>
 					</ol>
+					<h3 class="questions" align="center">There are 9 planets in our solar system
+						<br/><br/>
+						<button class="trueBut" onClick={this.falseClick}>True</button>
+						<button class="falseBut" onClick={this.trueClick}>False</button>
+					</h3>
+					<h3 class="questions" align="center">Mercury is the smallest planet
+						<br/><br/>
+						<button class="trueBut" onClick={this.trueClick}>True</button>
+						<button class="falseBut" onClick={this.falseClick}>False</button>
+					</h3>
+					<h3 class="questions" align="center">Jupiter is the largest planet
+						<br/><br/>
+						<button class="trueBut" onClick={this.trueClick}>True</button>
+						<button class="falseBut" onClick={this.falseClick}>False</button>
+					</h3>
 				</body>
 			</html>
     	);
