33ce1f93aafb5fb1c66f088f1aeecdc4e88c4cd0
Michael T <m3teitel@ryerson.ca>
2018-12-02 23:15:52 -0500

    commiting

--- src/Tutorial.js
+++ src/Tutorial.js
@@ -37,40 +37,7 @@ class Tutorial extends React.Component {
 					<body>
 						<p>
 							Y'all ready to learn how to make the best darn website on this side of the galaxy?
-						</p>
-						<script>
-						
-							var redditSubModule = "SpacePorn";
-							var http = require('http');
-							var name = "";
-							function getRedditPosts() {
-								var url = "http://www.reddit.com/r/" + redditSubModule + "/new/.json?limit=50";
-								var request = http.get(url, function(response) {
-									var json = '';
-									response.on('data', function(chunk) {
-										json += chunk;
-									});
-									response.on('end', function() {
-										var redditResponse = JSON.parse(json);
-										redditResponse.data.children.forEach(function(child) {
-											if(child.data.domain !== 'self.node') {
-												this.name += '-------------------------------';
-												this.name += 'Author : ' + child.data.author;
-												this.name += 'Domain : ' + child.data.domain;
-												this.name += 'Title : ' + child.data.title;
-												this.name += 'URL : ' + child.data.url;
-											}
-										});
-									})
-								});
-								request.on('error', function(err) {
-									console.log(err);
-								});
-							}
-							getRedditPosts();
-							document.getElementById("test").innerHTML = name;
-						</script>
-						
+						</p>						
 						<div id="test"></div>
 						<br/>
 					</body>
